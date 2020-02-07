f4fd584a913b76504e3c384451d855ee76ec72f4
Mitchell Robles <mitchellr92@live.com>
2020-02-07 09:38:23 -0800

    Saved selected data to local storage

--- MovieSeatBooking/main.js
+++ MovieSeatBooking/main.js
@@ -6,9 +6,19 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -17,10 +27,11 @@ function updateSelectedCount() {
 }
 
 //Movie select event
-movieSelect.addEventListener('change', e => {
-  ticketPrice = +e.target.value
-  updateSelectedCount()
-})
+movieSelect.addEventListener("change", e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
 
 // Seat click event
 container.addEventListener("click", e => {
