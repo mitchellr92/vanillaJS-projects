455d8bf7fd4432fc6536fdaa7b1d3580968c576b
Mitchell Robles <mitchellr92@live.com>
2020-04-09 16:18:47 -0700

    Wrong letters and play again

--- HangmanGame/main.js
+++ HangmanGame/main.js
@@ -38,7 +38,30 @@ function displayWord() {
 }
 
 // Update the wrong letters
-function updateWrongLettersEl() {}
+function updateWrongLettersEl() {
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wring</p>" : ""}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  //Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost...";
+    popup.style.display = "flex";
+  }
+}
 
 // Show notification
 function showNotification() {
@@ -72,6 +95,21 @@ window.addEventListener("keydown", e => {
       }
     }
   }
+});
+
+// Play again
+playAgainBtn.addEventListener("click", () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = "none";
 });
 
 displayWord();
