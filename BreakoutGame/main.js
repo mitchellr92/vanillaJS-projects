0863914a804ff88a05e4e1f350d16efa945f1843
Mitchell Robles <mitchellr92@live.com>
2020-04-13 09:44:55 -0700

    open and close game rules

---
+++ BreakoutGame/main.js
@@ -0,0 +1,13 @@
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+
+// Event handlers
+rulesBtn.addEventListener("click", () => {
+  rules.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  rules.classList.remove("show");
+});
