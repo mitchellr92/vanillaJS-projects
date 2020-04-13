9ca3f4f8ca17cf54d3a8c9b55b7c00f0b79a5c4a
Mitchell Robles <mitchellr92@live.com>
2020-04-13 10:42:54 -0700

    move paddle

--- BreakoutGame/main.js
+++ BreakoutGame/main.js
@@ -25,7 +25,7 @@ const paddle = {
   y: canvas.height - 20,
   w: 80,
   h: 10,
-  speed: 10,
+  speed: 8,
   dx: 0
 };
 
@@ -89,13 +89,61 @@ function drawBricks() {
 
 // Draw everything
 function draw() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
   drawPaddle();
   drawScore();
   drawBricks();
 }
 
-draw();
+// Move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
+// Update canvas drawing and animation
+function update() {
+  movePaddle();
+
+  // Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
 
 // Event handlers
 rulesBtn.addEventListener("click", () => {
@@ -105,3 +153,6 @@ rulesBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   rules.classList.remove("show");
 });
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
